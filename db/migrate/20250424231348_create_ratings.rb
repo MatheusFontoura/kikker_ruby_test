441b9d64647ac2bcc8bd6b32ef1cc74cff2fd777
Matheus Fontoura <matheus.fontoura@herospark.com>
2025-04-24 20:22:04 -0300

    Create migrations and schema

---
+++ db/migrate/20250424231348_create_ratings.rb
@@ -0,0 +1,14 @@
+class CreateRatings < ActiveRecord::Migration[7.1]
+  def change
+    create_table :ratings do |t|
+      t.references :post, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.integer :value, null: false
+
+      t.timestamps
+    end
+
+    add_index :ratings, [:post_id, :user_id], unique: true
+    add_check_constraint :ratings, 'value BETWEEN 1 AND 5', name: 'rating_value_range'
+  end
+end
