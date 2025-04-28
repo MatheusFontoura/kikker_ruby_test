12a0ec8fc623bfa41de852d99df042d241748c1e
Matheus Fontoura <matheus.fontoura@herospark.com>
2025-04-28 04:44:06 -0300

    adding readme and small adjustments

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,12 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-
 require 'faker'
 require 'typhoeus'
 require 'json'
+
+puts 'Limpando bancos de dados...'
+Rating.delete_all
+Post.delete_all
+User.delete_all
+puts 'Bancos de dados limpos!'
 
 TOTAL_USERS = 100
 TOTAL_POSTS = 200_000
@@ -24,12 +20,11 @@ API_URL = ENV.fetch('API_URL', 'http://web:3000')
 # 50 IPs
 ips = (1..50).map { |i| "192.168.0.#{i}" }
 
-Rails.logger.debug 'Criando usuários...'
+puts 'Criando usuários...'
 
 users = Array.new(TOTAL_USERS) { |i| "user_#{i}_#{SecureRandom.hex(3)}" }
 user_ids = []
 
-# rubocop:disable Metrics/BlockLength
 users.each_with_index do |login, index|
   body = {
     title: "Seed Post User #{index}",
@@ -51,25 +46,25 @@ users.each_with_index do |login, index|
     parsed = JSON.parse(response.body)
     user_ids << parsed['user']['id']
   else
-    Rails.logger.debug { "Erro criando usuário #{login}. Novamente..." }
+    puts "Erro criando usuário #{login}. Tentando novamente..."
     retry_response = request.run
     if retry_response.success?
       parsed = JSON.parse(retry_response.body)
       user_ids << parsed['user']['id']
     else
-      Rails.logger.debug { "Falha ao criar usuário #{login} após retry. Código: #{retry_response.code}" }
+      puts "Falha ao criar usuário #{login} após retry. Código: #{retry_response.code}"
     end
   end
 end
 
-Rails.logger.debug { "Usuários criados: #{user_ids.count}" }
+puts "Usuários criados: #{user_ids.count}"
 
-Rails.logger.debug 'Criando posts...'
+puts 'Criando posts...'
 
 posts_ids = []
 
 (0...TOTAL_POSTS).each_slice(POSTS_BATCH_SIZE) do |batch|
-  hydra = Typhoeus::Hydra.new(max_concurrency: 40)
+  hydra = Typhoeus::Hydra.new(max_concurrency: 30)
 
   batch.each do
     body = {
@@ -91,13 +86,13 @@ posts_ids = []
         parsed = JSON.parse(response.body)
         posts_ids << parsed['post']['id'] if parsed
       else
-        Rails.logger.debug 'Erro criando post. Tentando novamente...'
+        puts 'Erro criando post. Tentando novamente...'
         retry_response = request.run
         if retry_response.success?
           parsed = JSON.parse(retry_response.body)
           posts_ids << parsed['post']['id'] if parsed
         else
-          Rails.logger.debug { "Falha ao criar post após retry. Código: #{retry_response.code}" }
+          puts "Falha ao criar post após retry. Código: #{retry_response.code}"
         end
       end
     end
@@ -106,15 +101,15 @@ posts_ids = []
   end
 
   hydra.run
-  Rails.logger.debug { "Batch de #{batch.size} posts criada!" }
+  puts "Batch de #{batch.size} posts criada!"
 end
 
-Rails.logger.debug { "Total de posts criados: #{posts_ids.size}" }
+puts "Total de posts criados: #{posts_ids.size}"
 
-Rails.logger.debug 'Criando ratings...'
+puts 'Criando ratings...'
 
 posts_ids.sample(TOTAL_RATINGS).each_slice(RATINGS_BATCH_SIZE) do |batch|
-  hydra = Typhoeus::Hydra.new(max_concurrency: 40)
+  hydra = Typhoeus::Hydra.new(max_concurrency: 30)
 
   batch.each do |post_id|
     user_id = user_ids.sample
@@ -134,10 +129,10 @@ posts_ids.sample(TOTAL_RATINGS).each_slice(RATINGS_BATCH_SIZE) do |batch|
 
     request.on_complete do |response|
       unless response.success?
-        Rails.logger.debug 'Erro criando rating. Tentando novamente...'
+        puts 'Erro criando rating. Tentando novamente...'
         retry_response = request.run
         unless retry_response.success?
-          Rails.logger.debug { "Falha ao criar rating após retry. Código: #{retry_response.code}" }
+          puts "Falha ao criar rating após retry. Código: #{retry_response.code}"
         end
       end
     end
@@ -146,8 +141,7 @@ posts_ids.sample(TOTAL_RATINGS).each_slice(RATINGS_BATCH_SIZE) do |batch|
   end
 
   hydra.run
-  Rails.logger.debug { "Batch de #{batch.size} ratings criado" }
+  puts "Batch de #{batch.size} ratings criado!"
 end
-# rubocop:enable Metrics/BlockLength
 
-Rails.logger.debug 'Seeds feitas'
+puts 'Seeds feitas!'
